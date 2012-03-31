db8ed4bfa05dda0b1781f7aa505344d03ae92b4e
Gabriel Puado <jgpuado@gmail.com>
2012-03-31 20:12:29 +0200

    Solucionados errores varios y añadida relacion N a N

--- bases.rb
+++ bases.rb
@@ -10,39 +10,56 @@ MY_DB = SQLite3::Database.new(MY_DB_NAME)
 ActiveRecord::Base.establish_connection(:adapter => 'sqlite3', :database => MY_DB_NAME)
 
 class Cookbook < ActiveRecord::Base
-    #has_one :enviroment
-    #self.primary_key= name
+    has_and_belongs_to_many :enviroments
+
     self.connection.create_table(:cookbooks,:force=>true) do |t|
         t.column :name, :string, :null=>false, :unique=>true
         t.column :path, :string
         # Parece ser que type esta reservado por ruby, cambiado por place
         t.column :place, :string, :default=>'L'
+        t.column :enviroments, :enviroment, :foreign_key=>true
     end
+    
     validates_uniqueness_of :name
     # Obliga a que el campo :place sea R o L
     validates :place, :inclusion => {:in=> ['R', 'L'], :message=> "%{value} no es un valor correcto" }
 end
 
-class Env_db < ActiveRecord::Base
-    belongs_to :cookbook
-    #set_table_name :enviroments
-    self.table_name= 'enviroments'
-    #self.primary_key= 'id'
-    self.connection.create_table(:enviroments,:force=>true) do |t|
+
+class Enviroment < ActiveRecord::Base
+    has_and_belongs_to_many :cookbooks
+    
+    self.connection.create_table(:enviroments, :force=>true) do |t|
         # El identificador autonumerado se crea automaticamente
-        t.column :name, :string, :default=>'env-'+ (last.id-1).to_s
+        t.column :name, :string, :default=>'env-' #+ (last.id-1).to_s
         t.column :ssh, :string, :default=>nil
-        t.column :cookbooks, :cookbook
+        t.column :cookbooks, :cookbook, :foreign_key=> true #, :default=>nil
     end
    
+end
+
+class CreateSchema < ActiveRecord::Migration
+    create_table :cookbooks_enviroments, :id=>false do |t|
+        t.references :cookbook
+        t.references :enviroment
+    end
 end
 
 #=begin
 Cookbook.create(:name=>'emacs', :path=>'/ruta/hacia/emacs')
 Cookbook.create(:name=>'vim', :path=>'/ruta/hacia/vim')
 Cookbook.create(:name=>'apache', :path=>'/ruta/hacia/apache')
-#=begin
-Env_db.create(:name=>'nombre1', :ssh=>'clave1', :cookbooks => Cookbook.find(2))
-Env_db.create(:ssh=>'clave2')
-Env_db.create(:name=>'nombre3', :ssh=>'clave3', :cookbooks => Cookbook.first(:conditions => {:name => 'emacs'}))
 #=end
+#=begin
+Enviroment.create(:name=>'nombre1', :ssh=>'clave1') #, :cookbooks => Cookbook.find(2))
+Enviroment.create(:ssh=>'clave2')
+Enviroment.create(:name=>'nombre3', :ssh=>'clave3') #, :cookbooks => Cookbook.first(:conditions => {:name => 'emacs'}))
+#=end
+
+=begin
+ent1 = Env_db.create(:ssh=>'clave1')
+ent2 = Env_db.create(:ssh => 'clave2')
+
+ent1.cookbooks.create(:name=>'vim', :path=>'/ruta/hacia/vim')
+ent1.cookbooks.create(:name=>'emacs', :path=>'/ruta/hacia/emacs')
+=end
