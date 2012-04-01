f15d0ddd381d2fd484a476e5f5da3d6df7afbb2e
Gabriel Puado <jgpuado@gmail.com>
2012-04-01 14:45:30 +0200

    Uso de serialize para serializar el objeto Descripcion. Solo hay que pasarle el objeto a la base de datos, y esta ya se encarga de serializarlo como YAML sin necesidad de implementar nada.

--- bases.rb
+++ bases.rb
@@ -9,6 +9,32 @@ MY_DB = SQLite3::Database.new(MY_DB_NAME)
 # get active record set up
 ActiveRecord::Base.establish_connection(:adapter => 'sqlite3', :database => MY_DB_NAME)
 
+#Pongo esto aquí de forma provisional para no tocarle nada a bae
+class EnvDescription
+	attr_accessor :name, :image, :ssh, :type, :network, :vnc
+	
+	def initialize(name, image, ssh, type, network, vnc)
+		@name = name
+		@image = image
+		#@cookbooks = cookbooks
+		@ssh = ssh
+		@type = type
+		@network = network
+		@vnc = vnc
+	end
+	
+	def to_s
+		str = "Name :" + @name.to_s + "\n"  
+		str += "Image :" + @image.to_s + "\n"
+		str += "SSH :" + @ssh.to_s + "\n"
+		str += "Type :" + @type.to_s + "\n"
+		str += "Network :" + @network.to_s + "\n"
+		str += "VNC :" + @vnc.to_s + "\n"
+		str
+	end
+
+end
+
 class Cookbook < ActiveRecord::Base
     has_and_belongs_to_many :enviroments 
     validates_uniqueness_of :name
@@ -19,6 +45,7 @@ end
 
 class Enviroment < ActiveRecord::Base
     has_and_belongs_to_many :cookbooks
+    serialize :description
 end
 
 class CreateSchema < ActiveRecord::Migration
@@ -34,7 +61,7 @@ class CreateSchema < ActiveRecord::Migration
     create_table(:enviroments, :force=>true) do |t|
         # El identificador autonumerado se crea automaticamente
         t.column :name, :string, :default=>'env-' #+ (last.id-1).to_s
-        t.column :ssh, :string, :default=>nil
+        t.column :description, :string, :default=>nil
         t.column :cookbooks, :cookbook #, :foreign_key=> true #, :default=>nil
     end
 
@@ -44,15 +71,18 @@ class CreateSchema < ActiveRecord::Migration
     end
 end
 
+e1 = EnvDescription.new('env1',8,'clave1','small','public',true)
+e2 = EnvDescription.new('env2',7,'clave2','small','public',true)
+e3 = EnvDescription.new('env3',12,'clave3','small','public',true)
 #=begin
 Cookbook.create(:name=>'emacs', :path=>'/ruta/hacia/emacs')
 Cookbook.create(:name=>'vim', :path=>'/ruta/hacia/vim')
 Cookbook.create(:name=>'apache', :path=>'/ruta/hacia/apache')
 #=end
 #=begin
-Enviroment.create(:name=>'nombre1', :ssh=>'clave1') #, :cookbooks => Cookbook.find(2))
-Enviroment.create(:ssh=>'clave2')
-Enviroment.create(:name=>'nombre3', :ssh=>'clave3') #, :cookbooks => Cookbook.first(:conditions => {:name => 'emacs'}))
+Enviroment.create(:name=>'nombre1', :description => e1) #, :cookbooks => Cookbook.find(2))
+Enviroment.create(:description => e2)
+Enviroment.create(:name=>'nombre3', :description=> e3) #, :cookbooks => Cookbook.first(:conditions => {:name => 'emacs'}))
 #=end
 
 =begin
@@ -62,3 +92,11 @@ ent2 = Env_db.create(:ssh => 'clave2')
 ent1.cookbooks.create(:name=>'vim', :path=>'/ruta/hacia/vim')
 ent1.cookbooks.create(:name=>'emacs', :path=>'/ruta/hacia/emacs')
 =end
+
+desc1 = Enviroment.find(1).description
+desc2 = Enviroment.find(2).description
+desc3 = Enviroment.find(3).description
+
+puts desc1.to_s
+puts desc2.to_s
+puts desc3.to_s
