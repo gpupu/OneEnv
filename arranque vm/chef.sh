5cd07693b2ee6995ef88fefe4403e72566992923
Gabriel Puado <jgpuado@gmail.com>
2012-04-27 17:39:07 +0200

    Preparando para arrancar desde la VM

--- arranque vm/chef.sh
+++ arranque vm/chef.sh
@@ -1,23 +1,33 @@
 #!/bin/bash
 
+readonly CHPATH="/tmp/chef/"
 readonly JPATH="/tmp/chef/node.json"
 readonly CPATH="/tmp/chef/solo.rb"
 
 # Crea el directorio
-mkdir -pv /tmp/chef
+mkdir -pv $CHPATH
+
 
 # Crea el archivo json
 #echo "{\"run_list\": [\"recipe[emacs]\"]}" > /etc/chef/node.json
-echo -n "{\"run_list\": [\"recipe[" > $JPATH
-echo -n $CHEF_RECIPE >> $JPATH
-echo "]\"]}" >> $JPATH
+#echo -n "{\"run_list\": [\"recipe[" > $JPATH
+#echo -n $CHEF_RECIPE >> $JPATH
+#echo "]\"]}" >> $JPATH
+
 
 # Crea el archivo solo.rb
+echo "creando archivo configuracion"
 echo "file_cache_path \"/tmp/chef\"" > $CPATH
 echo "cookbook_path \"/tmp/chef/cookbooks\"" >> $CPATH
+echo "role_path \"/tmp/chef/roles\"" >> $CPATH
+echo "data_bag_path \"/tmp/chef/data_bags\"" >> $CPATH
 
-# Copia las recetas al disco
-cp -rv /mnt/recipes/ /tmp/chef/cookbooks/
+
+# Copia todo al disco
+echo "copiando elementos al disco"
+cp -rv /mnt/cookbooks/ /tmp/chef/cookbooks/
+cp -rv /mnt/roles/ /tmp/chef/roles
+cp -rv /mnt/data_bags/ /tmp/chef/data_bags/
 
 # Ejecuta Chef-solo
 echo "Ejecuta chef-solo"
--- arranque vm/init.sh
+++ arranque vm/init.sh
@@ -6,5 +6,10 @@
 #echo "Instalando vim"
 #sudo apt-get -y install vim
 
+mount -t iso9660 /dev/vdc /mnt
+
 . ./mnt/chef.sh >/var/log/cheflog 2>&1
 
+umount /mnt
+
+exit 0
