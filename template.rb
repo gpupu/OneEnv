06a49c9208f922661d3df3dccd17b33e3e2e611c
baena5999 <baena5999@gmail.com>
2012-04-27 19:07:21 +0200

    template encapsulada en clase

--- template.rb
+++ template.rb
@@ -1,15 +1,12 @@
-##PRECONDICION:
-	## Nombre entorno: Cadena caractes
-	## Numero Template: Numero
-	## Cookbooks: Array con PATH de los Cookbooks
-	## Node.joson: Archivo correcto para la contextualizacion
-##PARAMETROS: Nombre Entorno, Numero Template, Cookbooks, Node.json
-
-##POSTCONDICION: 
-	#Creada una template en formato XML lista para instanciar
-
 #!/usr/bin/env ruby
+
  
+##############################################################################
+# Required libraries
+##############################################################################
+
+
+
 ##############################################################################
 # Environment Configuration
 ##############################################################################
@@ -22,66 +19,117 @@ else
 end
  
 $: << RUBY_LIB_LOCATION
- 
-##############################################################################
-# Required libraries
-##############################################################################
+
 require 'OpenNebula'
- 
 include OpenNebula
- 
+
 # OpenNebula credentials
 CREDENTIALS = "oneadmin:nebula"
 # XML_RPC endpoint where OpenNebula is listening
 ENDPOINT    = "http://localhost:2633/RPC2"
- 
-client = Client.new(CREDENTIALS, ENDPOINT)
 
-template_pool = TemplatePool.new(client, -1)
- 
-rc = template_pool.info
-if OpenNebula.is_error?(rc)
-     puts rc.message
-     exit -1
-end
 
-=begin
-nom_entorno = ARGV[0]
-num_template = ARGV[1]
-cookbooks = ARGV[2]
-json = ARGV[3]
-=end
+class ConectorONE
 
-num_template = ARGV[0]
+:client
 
-xml_s=""
+		 
+	def initialize 
 
-template_pool.each do |tmp|
-	if tmp['ID'] == num_template
-		xml_s = tmp.to_xml
+		@client = Client.new(CREDENTIALS, ENDPOINT)
+	
+		
 	end
+
+
+	def crearTemplate(num_template,path_repo,path_json)
+
+		xml_s=""
+
+		template_pool= TemplatePool.new(@client, -1)
+		rc= template_pool.info
+
+		if OpenNebula.is_error?(rc)
+			  puts "#{rc.message}"   
+		end
+
+		template_pool.each do |tmp|	
+			if tmp['ID'] == num_template.to_s
+				xml_s = tmp.to_xml
+
+			end
+		end
+		
+
+	if xml_s!=""
+
+
+
+		xml =XMLElement.build_xml(xml_s, "VMTEMPLATE")
+		doc = Nokogiri::XML::Document.new
+	
+		files = path_repo + " " + path_json
+		target= "vdc"
+
+		###EDITAR CONTEXTO
+		if !xml.xpath("//VMTEMPLATE//TEMPLATE//CONTEXT").empty?
+			if !xml.xpath("//VMTEMPLATE//TEMPLATE//CONTEXT//FILES").empty?
+				puts xml.xpath("//VMTEMPLATE//CONTEXT//FILES")
+			else
+				puts "NO HAY FILES"		
+				node_files=Nokogiri::XML::Node.new("FILES", doc)
+				data_files=data=Nokogiri::XML::CDATA.new(doc,files)
+		
+				node_files << data_files
+				xml.xpath("//VMTEMPLATE//TEMPLATE//CONTEXT").first << node_files
+				puts xml
+			end
+		else
+			puts "NO HAY CONTEXT"
+		
+			node_context=Nokogiri::XML::Node.new("CONTEXT", doc)
+			node_files=Nokogiri::XML::Node.new("FILES", doc)
+			node_target=Nokogiri::XML::Node.new("TARGET", doc)
+
+			data_target=data=Nokogiri::XML::CDATA.new(doc,target)		
+			data_files=data=Nokogiri::XML::CDATA.new(doc,files)
+		
+			node_target << data_target
+			node_files << data_files
+
+			node_context << node_target
+			node_context << node_files
+			xml.xpath("//VMTEMPLATE//TEMPLATE").first << node_context
+			puts xml
+		
+		end
+
+	
+	
+
+		template = Template.new(xml,@client)
+		xml_string = template.template_str
+
+
+
+		vm = VirtualMachine.new(VirtualMachine.build_xml,@client)
+		rc = vm.allocate(xml_string)
+
+
+
+		if OpenNebula.is_error?(rc)
+			  puts "#{rc.message}"   
+		end
+
+
+		end
+
+
+	end
+
 end
-
-if xml_s!=""
-xml =XMLElement.build_xml(xml_s, "VMTEMPLATE")
-puts xml.class
-
-
-template = Template.new(xml,client)
-###EDITAR CONTEXTO
-xml_string = template.template_str
-
-
-
-vm = VirtualMachine.new(VirtualMachine.build_xml,client)
-rc = vm.allocate(xml_string)
-
-
-
-if OpenNebula.is_error?(rc)
-          puts "#{rc.message}"   
-end
-end
-exit 0
-
+=begin
+c= ConectorONE.new
+c.crearTemplate(10,"/var/lib/one/init.sh /home/david/solodemo/cookbooks","")
+=end
 
