365a3eae473f9084dadda84562b9ac784a66868b
Gabriel Puado <jgpuado@gmail.com>
2012-06-12 20:56:05 +0200

    Apache License

--- lib/oneenv/template.rb
+++ lib/oneenv/template.rb
@@ -1,4 +1,18 @@
-#!/usr/bin/env ruby
+# --------------------------------------------------------------------------#
+# Copyright 2012   David Baena, Fernando Martínez-Conde, José Gabriel Puado	#
+# 																			#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may 	#
+# not use this file except in compliance with the License. You may obtain 	#
+# a copy of the License at 													#
+# 																			#
+# http://www.apache.org/licenses/LICENSE-2.0 								#
+# 																			#
+# Unless required by applicable law or agreed to in writing, software 		#
+# distributed under the License is distributed on an "AS IS" BASIS, 		#
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 	#
+# See the License for the specific language governing permissions and 		#
+# limitations under the License. 											#
+#---------------------------------------------------------------------------#
 
  
 ##############################################################################
@@ -13,7 +27,6 @@ end
 SCRIPT_DIR=LIB_LOCATION+"/sh/start_env"
 
 
-#require 'nokogiri/XML'
 require 'OpenNebula'
 include OpenNebula
 
@@ -22,9 +35,6 @@ include OpenNebula
 # Environment Configuration
 ##############################################################################
 
-# OpenNebula credentials
-#CREDENTIALS = "oneadmin:nebula"
-# XML_RPC endpoint where OpenNebula is listening
 ENDPOINT    = "http://localhost:2633/RPC2"
 
 
@@ -193,10 +203,3 @@ class ConectorONE
 
 
 end
-
-=begin
-c= ConectorONE.new
-c.crearTemplate(10,"/var/lib/one/init.sh /home/david/solodemo/cookbooks","")
-=end
-
-
